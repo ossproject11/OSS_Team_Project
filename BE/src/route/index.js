139ef686edef7c832e127c21706fa251b6337092
bingoring <ritch1530@gmail.com>
2021-10-28 17:01:12 +0900

    feature/get user info

--- BE/src/route/index.js
+++ BE/src/route/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const auth = require("./auth");
 const register = require("./register");
 const checkDuplicate = require("./checkDuplicate");
-
+const getUserInfo = require("./getUserInfo");
 const router = express.Router();
 
 router.use("/auth", auth);
 router.use("/register", register);
 router.use("/checkDuplicate", checkDuplicate);
+router.use("/getUserInfo", getUserInfo);
 
 module.exports = router;
