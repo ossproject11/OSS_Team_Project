56b2607d09fca9a017e23142aead50de0b22e847
Hmgi <hk335078@naver.com>
2021-11-01 17:11:28 +0900

    데이터 업로드 스크립트 코드 변경

--- Data/UploadData/api_upload_db.py
+++ Data/UploadData/api_upload_db.py
@@ -16,9 +16,6 @@ for i in range(len(performance_id)):
     a_list = requests.get("http://www.kopis.or.kr/openApi/restful/pblprfr/" + performance_id[i].text + "?service=9a8ded23eba14870b33c1ff1ad746a83")
     a_soup = BeautifulSoup(a_list.content, "html.parser")
 
-    data = []
-    data.append(performance_id[i].text)
-
     # performance_id[i]                         # 공연 ID
     a_id = a_soup.find_all("mt10id")            # 공연 시설 ID
     b_id = a_soup.find_all("prfnm")             # 공연 명
@@ -36,14 +33,40 @@ for i in range(len(performance_id)):
     n_id = a_soup.find_all("genrenm")           # 공연 장르
     o_id = a_soup.find_all("prfstate")          # 공연 상태
     p_id = a_soup.find_all("openrun")           # 공연 오픈런
-    #q_id = a_soup.find_all("styurls")           # 공연 소개이미지목록
+    #q_id = a_soup.find_all("styurls")          # 공연 소개이미지목록
     #if len(q_id) == 0:
     #    q_id = ["<styurls>x</styurls>"]
     r_id = a_soup.find_all("dtguidance")        # 공연 시간
 
-    sql = "insert into Details values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    sql = "insert into Details values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
     curs.execute(sql, (performance_id[i].text, a_id[0].text, b_id[0].text, c_id[0].text, d_id[0].text, e_id[0].text, f_id[0].text, g_id[0].text, h_id[0].text,
-                       i_id[0].text, j_id[0].text, k_id[0].text, l_id[0].text, m_id[0].text, n_id[0].text, o_id[0].text, p_id[0].text, r_id[0].text))
+                       i_id[0].text, j_id[0].text, k_id[0].text, l_id[0].text, m_id[0].text, n_id[0].text, o_id[0].text, p_id[0].text, r_id[0].text, "test"))
     conn.commit()
+
+# 공연시설상세 api 추출
+fcl_list = requests.get("http://www.kopis.or.kr/openApi/restful/prfplc?service=3dbea193a9e0445a9c80d813e9233d93&cpage=1&rows=1448".encode('utf-8'))
+fcl_soup = BeautifulSoup(fcl_list.content, "html.parser")
+facility_id = fcl_soup.find_all("mt10id")
+
+for i in range(len(facility_id)):
+    a_list = requests.get("http://www.kopis.or.kr/openApi/restful/prfplc/" + str(
+        facility_id[i].text) + "?service=9a8ded23eba14870b33c1ff1ad746a83")
+    a_soup = BeautifulSoup(a_list.content, "html.parser")
+
+    a_id = a_soup.find_all("fcltynm")  # 공연 시설명
+    b_id = a_soup.find_all("mt13cnt")  # 공연장 수
+    c_id = a_soup.find_all("fcltychartr")  # 시설 특성
+    d_id = a_soup.find_all("opende")  # 개관연도
+    e_id = a_soup.find_all("seatscale")  # 객석 수
+    f_id = a_soup.find_all("telno")  # 전화번호
+    g_id = a_soup.find_all("relateurl")  # 홈페이지
+    h_id = a_soup.find_all("adres")  # 주소
+    i_id = a_soup.find_all("la")  # 위도
+    j_id = a_soup.find_all("lo")  # 경도
+
+    sql = "insert into Details values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    curs.execute(sql, (facility_id[i].text, a_id[0].text, b_id[0].text, c_id[0].text, d_id[0].text, e_id[0].text, f_id[0].text, g_id[0].text, h_id[0].text, i_id[0].text, j_id[0].text))
+    conn.commit()
+
 curs.close()
 conn.commit()
